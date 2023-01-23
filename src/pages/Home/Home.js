9749e2fc5d8fa7583abbd21da1d4dba768b8c62b
LudmilaS <ludmila.https@gmail.com>
2023-01-22 22:27:22 -0300

    pronto

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,49 +1,60 @@
 import React from 'react'
-import { HistoricScreen } from './style'
+import { HistoricScreen, Feed, Transfers, Balance } from './style'
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import { UserContext } from "../../UserContext.js"
-import { useEffect, useState, useContext} from 'react'
+import { useContext, useEffect } from 'react'
+import axios from "axios"
 
-function Home(){
-    const { info, historic, setInfo, setHistoric} = useContext(UserContext)
-    const [userName, setUserName] = useState("Stranger")
-    const [userTransactions, setUserTransactions] = useState([])
+function Home() {
+    const { info, historic, setHistoric } = useContext(UserContext)
 
-    function calc() {
-        let balance = 0
-        userTransactions.forEach(transaction => {
-            balance += Number(transaction.value)
-        })
-        return String(balance)
+    useEffect(() => {
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${info.token}`
+                }
+            }
+            const promise = axios.get(`${process.env.REACT_APP_API_URL}/post`, config);
+            promise.then(res => setHistoric(res.data))
+
+        } catch (error) {
+            if (error.name === "AxiosError") alert("We couldn't find an account with this data!")
+        }
+    }, [])
+
+    let sale = 0;
+
+    for (let i = 0; i < historic.length; i++) {
+        if (historic[i].type === 'enter') {
+            sale = sale + Number(historic[i].value);
+        } else {
+            sale = sale - Number(historic[i].value);
+        }
     }
 
-    return(
+    return (
         <HistoricScreen>
-            <Header info={info}/>
-            <section aux={userTransactions.length}>
-                    {userTransactions.length ? (
-                        <>
-                            <feed>
-                                {userTransactions.map(transaction => (
-                                    <transaction>
-                                        <info>
-                                            <h2>{transaction.date}</h2>
-                                            <h3>{transaction.description}</h3>
-                                        </info>
-                                        <div>
-                                            <transactionValue transactionType={transaction.type}>{transaction.value}</transactionValue>
-                                        </div>
-                                    </transaction>
-                                ))}
-                            </feed>
-                            <value>
-                                <balanceText>VALUE</balanceText>
-                                <balanceValue>{calc()}</balanceValue>
-                            </value>
-                        </>
-                    ) : <p>There are no entry or exit records</p>}
-                </section>
+            <Header info={info} />
+            <>{historic.length ? (
+                <Feed>
+                    <Transfers >
+                        {historic.map((transf) =>
+                            <section>
+                                <h1>{transf.date}</h1>
+                                <h2>{transf.description}</h2>
+                                {transf.type === 'enter' ? <h3>{transf.value}</h3> : <h4>{transf.value}</h4>}
+                            </section>
+                        )}
+                    </Transfers>
+                    <Balance>
+                        <p>BALANCE</p>
+                        {sale < 0 ? <h3>{sale}</h3> : <h4>{sale}</h4>}
+
+                    </Balance>
+                </Feed>
+            ) : <h1>There are no entry or exit records</h1>}</>
             <Footer />
         </HistoricScreen>
     )
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,12 +4,14 @@ export const HistoricScreen = styled.div
 `
     background-color: #FFF;
     display: flex;
+    position: fixed;
     flex-direction: column;
-    width: 446px;
-    height: 100%;
-    min-height: 580px;
-    padding: 28px 18px 0 18px;
-    margin-top: 8px;
+    width: 100%;
+    height: 80%;
+    left: 0;
+    padding: 0;
+    border-radius: 5px;
+    border: 25px solid #8C11BE;
     display: flex;
     align-items: center;
     justify-context: center;
@@ -17,17 +19,102 @@ export const HistoricScreen = styled.div
         font-family: 'Lexend Deca';
         font-size: 23px;
         color: #126BA5;
+        margin-top: 17px;
         margin-bottom: 17px;
     }
-    >section {
-        > p {
+`
+export const Feed= styled.div
+`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-context: center;
+    flex-direction: column;
+`
+
+export const Transfers= styled.div
+`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-context: center;
+    flex-direction: column;
+    padding: 15px;
+    > section{
+        width: 100%;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        > h1{
             font-family: 'Raleway';
-            font-style: normal;
             font-weight: 400;
-            font-size: 20px;
-            line-height: 23px;
-            text-align: center;
-            color: #868688;
+            font-size: 22px;
+            line-height: 22px;
+            color: #C6C6C6;
+            padding: 5px;
         }
+        > h2{
+            font-family: 'Raleway';
+            font-weight: 400;
+            font-size: 22px;
+            line-height: 22px;
+            color: #000000;
+        }
+        > h3{
+            font-family: 'Raleway';
+            font-weight: 400;
+            font-size: 22px;
+            line-height: 22px;
+            text-align: right;
+            color: #03AC00;
+        }
+        > h4{
+            font-family: 'Raleway';
+            font-weight: 400;
+            font-size: 22px;
+            line-height: 22px;
+            text-align: right;
+            color: #C70000;
+        }
+    }
+`
+
+export const Balance= styled.div
+`
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 35px;
+    padding-right: 35px;
+    padding-bottom: 10px;
+    > p{
+        font-family: 'Raleway';
+        font-weight: 700;
+        font-size: 22px;
+        line-height: 20px;
+        color: #000000;
+    }
+    > h3{
+        font-family: 'Raleway';
+        font-weight: 400;
+        font-size: 22px;
+        line-height: 20px;
+        text-align: right;
+        color: #C70000;
+    }
+    > h4{
+        font-family: 'Raleway';
+        font-weight: 400;
+        font-size: 22px;
+        line-height: 20px;
+        text-align: right;
+        color:#03AC00;
     }
 `
